cc8a66ad719e60df9f4ee6e48e204730d2a3274a
Dieckman, Eric <eric.dieckman@wisc.edu>
2019-02-25 10:19:58 -0600

    work done in class Wed Feb 20

---
+++ UWAdventure.WebApp/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UWAdventure.WebApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // view for creating new orders
+        public ActionResult NewOrder()
+        {
+            // need to eventually query our business layers to retrieve information to pre-populates parts of the form
+
+            return View();
+        }
+
+        public ActionResult CreateOrder(UWAdventure.WebApp.Models.NewOrderModel formdata)
+        {
+
+        }
+    }
+}
---
+++ UWAdventure.WebApp/Models/NewOrderModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UWAdventure.WebApp.Models
+{
+    /// <summary>
+    /// Model for receiving form data for new orders
+    /// </summary>
+    public class NewOrderModel
+    {
+        public int Store { get; set; }
+        public int SalesAssociate { get; set; }
+
+    }
+}
