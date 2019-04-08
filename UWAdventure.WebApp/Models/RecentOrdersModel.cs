35c917994322eb7b0601c5aa53f1816ea6f829cf
Dieckman, Eric <eric.dieckman@wisc.edu>
2019-04-08 09:37:34 -0500

    updates for in-class 4/8

---
+++ UWAdventure.WebApp/Models/RecentOrdersModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UWAdventure.Entities.ViewModels;
+
+namespace UWAdventure.WebApp.Models
+{
+    public class RecentOrdersModel
+    {
+        public IList<OrderViewModel> Orders { get; set; }
+        public int Days { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
